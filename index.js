c7b1892ae80991c38e6c0569435beb7c61daebc4
Dennis Akagha <d.akagha20@gmail.com>
2022-01-11 02:19:27 +0100

    Complete integration as per requirement

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ const authorInput = document.querySelector('#author');
 const booksContainer = document.querySelector('.books-container');
 
 // collection that keeps a list of books
-const collection = JSON.parse(localStorage.getItem('bookList')) || [
-  {
-    title: 'The Lions Den',
-    author: 'Dennis',
-  },
-  {
-    title: 'Peter Met Paul',
-    author: 'Dennis Akagha',
-  },
-];
+let collection = JSON.parse(localStorage.getItem('bookList')) || [];
 
 // create the book element
 const createBookelement = ({ title, author }) => {
@@ -22,48 +13,43 @@ const createBookelement = ({ title, author }) => {
     <p class="author">${author}</p>
     <button class="delete" type="button">Remove</button><br><br><hr>`;
   booksContainer.appendChild(booksList);
+
+  document.querySelectorAll('.delete').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const targetTitle = e.target.parentElement.firstChild.textContent;
+      const targetAuthor = e.target.previousElementSibling.textContent;
+      const targetEl = e.target.parentElement;
+      collection = collection.filter(
+        (book) => book.title !== targetTitle || book.author !== targetAuthor,
+      );
+      localStorage.setItem('bookList', JSON.stringify(collection));
+
+      targetEl.remove();
+    });
+  });
 };
 collection.forEach(createBookelement);
 
 // function to add a new book to the collection, with title and author.
 const addBook = (title, author) => {
-
-  collection.push({
-    title,
-    author,
-  });
-  localStorage.setItem('bookList', JSON.stringify(collection));
+  if (!titleInput.value || !authorInput.value) {
+    // eslint-disable-next-line no-alert
+    alert('Please input values');
+  } else {
+    collection.push({
+      title,
+      author,
+    });
+    localStorage.setItem('bookList', JSON.stringify(collection));
+  }
   return { title, author };
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newBook = addBook(
-    titleInput.value,
-    authorInput.value,
-  );
-  if(!titleInput.value){
-    return;
-  }else{
-    createBookelement(newBook);
-    titleInput.value = '';
-    authorInput.value = '';
-  }
-  }
- );
-
-// function that deletes a book
-
-const deleteBtn = document.querySelectorAll('.delete');
-deleteBtn.forEach((btn) => {
-  btn.addEventListener('click', () => {
-  });
+  const newBook = addBook(titleInput.value, authorInput.value);
+  if (!titleInput.value) return;
+  createBookelement(newBook);
+  titleInput.value = '';
+  authorInput.value = '';
 });
-
-// deleteBtn.addEventListener('click', () => {
-
-//     console.log('hello world');
-// })
-// const deleteBook = ({title, author}) => {
-
-// }
