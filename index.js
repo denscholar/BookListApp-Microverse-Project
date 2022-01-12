a767be33a8017b8cab4d50f4e50fd045d5d53b18
Sumair <sumairqaisar992@gmail.com>
2022-01-12 14:41:05 +0500

    Modify remove function

--- index.js
+++ index.js
@@ -33,13 +33,15 @@ class Storage {
   }
 
   static removeFromStorage(id) {
+    console.log('remove from storage called')
     const bookList = Storage.getBookFromStorage();
-    bookList.forEach((book, index) => {
-      if (book.id === id) {
-        book.splice(index, 1);
-      }
-    });
-    localStorage.setItem('bookList', JSON.stringify(bookList));
+    // bookList.forEach((book, index) => {
+    //   if (book.id === id) {
+    //     bookList.splice(index, 1);
+    //   }
+    // });
+    const filteredBooks = bookList.filter(book => book.id != id)
+    localStorage.setItem('bookList', JSON.stringify(filteredBooks));
   }
 }
 
@@ -55,7 +57,7 @@ class DummyData {
     bookList.classList.add('table-row');
     bookList.innerHTML = `
     <td>"${book.title}" by <span>${book.author}</span></td>
-    <td><button class="delete">Remove</button></td>
+    <td><button class="delete" data-id=${book.id}>Remove</button></td>
     `;
     bookContainer.appendChild(bookList);
   }
@@ -94,4 +96,5 @@ form.addEventListener('submit', (e) => {
 // REMOVE A BOOK EVENTs
 bookContainer.addEventListener('click', (e) => {
   DummyData.removeBook(e.target);
+  Storage.removeFromStorage(e.path[0].attributes[1].value)
 });
